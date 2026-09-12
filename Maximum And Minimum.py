abc33c6040baa5e29f180d4a919044d063b14e6d
Ayush Singh <singhanupriya841@gmail.com>
2026-09-12 10:28:37 +0530

    Add files via upload

---
+++ Maximum And Minimum.py
@@ -0,0 +1,10 @@
+L=[11,9,13,21,7,32,5]
+minnum=L[0]
+maxnum=0
+for i in L:
+    if i>maxnum:
+        maxnum=i
+    if i<minnum:
+        minnum=i
+print("maximum number:",maxnum)
+print("minimum number:",minnum)

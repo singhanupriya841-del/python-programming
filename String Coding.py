f236c404ae0647c8fe64c985c036d7c12be1f87f
Ayush Singh <singhanupriya841@gmail.com>
2026-09-18 10:09:32 +0530

    Add files via upload

---
+++ String Coding.py
@@ -0,0 +1,15 @@
+a=[]
+b=[]
+c=[]
+for i in range(0,50):
+    a.append(i)
+print(a)
+for i in range(1,51):
+    b.append(i**2)
+print(b)
+for i in range(1,27):
+    c.append(chr(96+i)*i)
+print(c)
+    
+    
+
